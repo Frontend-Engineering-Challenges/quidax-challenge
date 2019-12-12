49ea2462e73e058c6bc51de832b8cf00084a3838
ofega <fegaeze@gmail.com>
2019-12-12 08:25:43 +0100

    done the hover feature

--- js/render.js
+++ js/render.js
@@ -20,6 +20,31 @@ const toggleSearch = () => {
     topNav.classList.toggle('alt-nav');
 }
 
+const toggleMoreDetails = () => {
+
+    // Defining variables within as these components are dynamically generated
+    const carouselCells = document.querySelectorAll('.carousel-cell');
+    const moreDetailsBtns = document.querySelectorAll('.more-details-btn');
+
+    moreDetailsBtns.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            carouselCells.forEach(cell => {
+                if(cell.contains(e.target)) {
+                    if(!cell.classList.contains('show-details')) {
+                        btn.innerHTML = '<img src="./assets/icons/close.svg" class="is-close height-100-pc" alt="Close Icon">';
+                        cell.classList.add('show-details');
+                    } else {
+                        btn.innerHTML = '<img src="./assets/icons/ellipses.svg" class="width-100-pc height-100-pc" alt="Ellipses Icon">';
+                        cell.classList.remove('show-details');
+                    }
+                } else {
+                    cell.classList.remove('show-details');
+                }
+            })
+        });
+    });
+}
+
 const toggleNav = () => {
     document.body.classList.toggle('open-drawer');
 }
@@ -33,16 +58,38 @@ const displayFeaturedBooks = () => {
     const fragment = document.createDocumentFragment();
 
     controller.getFeaturedBooks()
-        .forEach(book => {
+        .forEach(item => {
             const div = document.createElement('div'); 
             div.classList.add('carousel-cell');
             div.innerHTML = `
-                <img src="${book.image}" alt="${book.name} Book Cover">
+                <button class="more-details-btn">
+                    <img src="./assets/icons/ellipses.svg" class="width-100-pc height-100-pc" alt="Ellipses Icon">
+                </button>
+                <img src="${item.image}" alt="${item.name} Book Cover">
+                <div class="with-overlay dc-content">
+                    <p class="picotext mb-1 ${item.available ? 'co-primary' : 'co-red'}">${item.available ? 'Available' : 'Borrowed Out'}</p>
+                    <div class="mb-1">
+                        <h3 class="co-white book-title">${item.name}</h3>
+                        <p class="picotext co-white">${item.author.join(', ')} ${item.year ? "<br>" + item.year : ''}</p>
+                    </div>
+                    <div class="mb-1 show-pretabletup">
+                        <p class="picotext co-white"><span>Genre: </span> ${item.genre.join(', ')}</p>
+                        <p class="picotext co-white"><span>Labels: </span> ${item.labels.join(', ')}</p>
+                    </div>
+                    <div class="d-flx al-i-c j-c-sb dc-engagement">
+                        <div class="ratings is-bigger">
+                            <p class="picotext co-white">Ratings: ${Number.parseFloat(item.ratings).toFixed(1)}</p>
+                            <img src="./assets/icons/ratings.svg"  alt="Ratings Icon">
+                        </div>
+                    </div>
+                </div>
             `
+
             fragment.appendChild(div); 
         })
     
     slider.appendChild(fragment);
+    toggleMoreDetails();  
 }
 
 const displayBookGrid = (data, parentElem) => {
@@ -57,7 +104,7 @@ const displayBookGrid = (data, parentElem) => {
                 </div>
                 <div class="dc-content">
                     <p class="picotext mb-1 ${item.available ? 'co-primary' : 'co-red'}">${item.available ? 'Available' : 'Borrowed Out'}</p>
-                    <h3 class="co-black book-tilte">${item.name}</h3>
+                    <h3 class="co-black book-title">${item.name}</h3>
                     <p class="picotext co-black">${item.author.join(', ')} ${item.year ? "- " + item.year : ''}</p>
                     <p class="picotext co-black">${item.genre.join(', ')}</p>
                     <div class="d-flx al-i-c dc-engagement">
@@ -114,11 +161,16 @@ const displaySearchResults = (data, inputValueLength) => {
 
 module.exports.init = () => {
 
+    // Function calls
+    displayFeaturedBooks();
+    displayBookGrid(controller.getRecentBooks(), recentBooks);
+    displayBookGrid(controller.getAllBooks(), allBooks);
+
     // Mobile Search Toggle
     searchToggleBtns.forEach(btn => {
         btn.addEventListener('click', toggleSearch);
     });
-    
+
     // Off-Canvas toggle
     navToggleBtns.forEach(btn => {
         btn.addEventListener('click', toggleNav);
@@ -138,10 +190,4 @@ module.exports.init = () => {
         displaySearchResults(results, e.target.value.length);
     });
     searchInput.addEventListener('blur', toggleAutoComplete);
-
-
-    // Function calls
-    displayFeaturedBooks();
-    displayBookGrid(controller.getRecentBooks(), recentBooks);
-    displayBookGrid(controller.getAllBooks(), allBooks);
 }
